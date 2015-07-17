8b576b0676ec6507ada7923325969989797759cf
Robert Helmer <rhelmer@rhelmer.org>
2015-07-17 00:46:03 -0700

    split out server, use single-quote

--- data/crashmy.js
+++ data/crashmy.js
@@ -1,3 +1,6 @@
+var reports_server = 'https://crash-reports.mocotoolsstaging.net'
+var stats_server = 'https://crash-stats.mocotoolsstaging.net'
+
 function submitCrash(form, serverURL) {
     var xhr = new XMLHttpRequest();
 
@@ -9,34 +12,52 @@ function submitCrash(form, serverURL) {
     var multipart = '';
     for (var key in form) {
         multipart += '--' + boundary +
-            "\r\nContent-Disposition: form-data; name=" + key +
-            "\r\nContent-type: text/plain" +
-            "\r\n\r\n" + form[key] + "\r\n";
+            '\r\nContent-Disposition: form-data; name=' + key +
+            '\r\nContent-type: text/plain' +
+            '\r\n\r\n' + form[key] + '\r\n';
     }
-    multipart += "--"+boundary+"--\r\n";
+    multipart += '--'+boundary+'--\r\n';
 
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4) {
-            alert('Result: ' + xhr.responseText);
+            var response = xhr.responseText;
+            var crashid = response.split('=')[1];
+            var url = stats_server + '/report/index/' + crashid;
+
+            var br = document.createElement('br'); 
+            document.body.appendChild(br);
+            var a = document.createElement('a'); 
+            a.href = url;
+            a.target='_newtab'
+            a.appendChild(document.createTextNode(crashid));
+            document.body.appendChild(a);
+            var loading = document.getElementById('loading');
+            loading.remove();
         }
     };
+
+    xhr.addEventListener('progress', function(evt) {
+        var span = document.createElement('span'); 
+        span.id = 'loading';
+        var content = '';
+        span.appendChild(document.createTextNode('workin...'));
+        document.body.appendChild(span);
+
+    }, false);
 
     xhr.send(multipart);
 }
 
-document.getElementById('crashmyjs').addEventListener('click',
-    function() {
-        var form = {};
-        form.ProductName = document.getElementById('productname').value;
-        form.Version = document.getElementById('version').value;
+document.getElementById('crashmyjs').addEventListener('click', function() {
+    var form = {};
+    form.ProductName = document.getElementById('productname').value;
+    form.Version = document.getElementById('version').value;
 
-        try {
-            throw Error('Crash My JS');
-        } catch(e) {
-            form.JSException = e.stack;
-            submitCrash(form,
-                'https://crash-reports.mocotoolsstaging.net/submit');
-        }
+    try {
+        throw Error('Crash My JS');
+    } catch(e) {
+        form.JSException = e.stack;
+        submitCrash(form, reports_server + '/submit');
+    }
 
-    }, false
-);
+}, false);
